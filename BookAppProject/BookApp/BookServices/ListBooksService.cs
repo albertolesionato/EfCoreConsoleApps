f3ab11d809fc4644a28cda8bcf226c9ffee67f75
Moneer R. Alhashim <61453768+moneer-muntazah@users.noreply.github.com>
2022-10-02 16:09:59 +0300

    SortFilterPageOptions

---
+++ BookAppProject/BookApp/BookServices/ListBooksService.cs
@@ -0,0 +1,24 @@
+﻿
+namespace BookApp.BookServices;
+
+public class ListBooksService
+{
+    public ListBooksService(BookAppDbContext Context)
+    {
+        this.Context = Context;
+    }
+
+    private readonly BookAppDbContext Context;
+
+    public IQueryable<BookListDto> SortFilterPage
+        (SortFilterPageOptions options) {
+        return Context.Books
+            .AsNoTracking()
+            .MapBookToDto()
+            .OrderBooksBy(options.OrderByOption)
+            .FilterBooksBy(options.FilterBy, options.FilterValue)
+            .Page(options.PageNum - 1, options.PageSize);
+    }
+
+}
+
--- BookAppProject/BookApp/BookServices/SortFilterPageOptions.cs
+++ BookAppProject/BookApp/BookServices/SortFilterPageOptions.cs
@@ -1,8 +1,18 @@
 ﻿
+using BookApp.QueryObjects;
 
 namespace BookApp.BookServices;
 
 public class SortFilterPageOptions
 {
+    public int PageNum = 1;
+
+    public int PageSize = 10;
+
+    public OrderByOption OrderByOption { get; set; }
+
+    public BooksFilterBy FilterBy { get; set; }
+
+    public string FilterValue { get; set; } = null!;
 }
 
